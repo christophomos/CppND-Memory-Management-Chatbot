215ce21bba27cf90df4bcad64765c6c8dee4763f
Chris Clark <christophomos@gmail.com>
2022-02-06 00:16:37 +0800

    Now all chatbot moves are done with move instead of copy

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -34,7 +34,7 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot& chatbot)
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
     _chatBot = std::move(chatbot);
     _chatBot.SetCurrentNode(this);
@@ -42,8 +42,7 @@ void GraphNode::MoveChatbotHere(ChatBot& chatbot)
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(_chatBot);
-    //_chatBot.reset(); // invalidate pointer at source
+    newNode->MoveChatbotHere(std::move(_chatBot));
 }
 ////
 //// EOF STUDENT CODE
