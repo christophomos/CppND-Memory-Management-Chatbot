19d8182893c5ec21097848d169b09a2625a80f1a
Chris Clark <christophomos@gmail.com>
2022-02-05 15:49:16 +0800

    Complete task 4: edges are owned by parent nodes

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include "graphedge.h"
 #include "graphnode.h"
 
@@ -25,9 +27,9 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(edge);
+    _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
@@ -51,7 +53,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return _childEdges[index].get();
 
     ////
     //// EOF STUDENT CODE
